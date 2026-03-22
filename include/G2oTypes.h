2d590ed66d80c6f5ab84c62c3a19bdfcacc4f342
elkuno213 <vuhoi.huynh@gmail.com>
2026-03-22 16:24:26 +0000

    refactor(build): remove Thirdparty/ prefix from g2o and DBoW2 includes
    - remove $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}> from root CMakeLists
    - update g2o/DBoW2 install destinations to drop Thirdparty/ prefix
    - rewrite 27 #include directives across 11 source files to match

--- include/G2oTypes.h
+++ include/G2oTypes.h
@@ -23,11 +23,11 @@
 #include <fstream>
 #include <vector>
 #include <Eigen/Core>
-#include <Thirdparty/g2o/g2o/core/base_binary_edge.h>
-#include <Thirdparty/g2o/g2o/core/base_multi_edge.h>
-#include <Thirdparty/g2o/g2o/core/base_unary_edge.h>
-#include <Thirdparty/g2o/g2o/core/base_vertex.h>
-#include <Thirdparty/g2o/g2o/types/types_sba.h>
+#include <g2o/g2o/core/base_binary_edge.h>
+#include <g2o/g2o/core/base_multi_edge.h>
+#include <g2o/g2o/core/base_unary_edge.h>
+#include <g2o/g2o/core/base_vertex.h>
+#include <g2o/g2o/types/types_sba.h>
 #include "ImuTypes.h"
 
 namespace ORB_SLAM3 {
